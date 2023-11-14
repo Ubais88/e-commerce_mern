dac3bbeb1ca17a26b967442bc41a01be38eaf835
Ubais88 <moubais2143@gmail.com>
2023-11-14 17:17:44 +0530

    login and signup page added

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../features/Navbar'
+import ProductList from '../features/product-list/ProductList'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar>
+            <ProductList></ProductList>
+        </Navbar>
+    </div>
+  )
+}
+
+export default Home
